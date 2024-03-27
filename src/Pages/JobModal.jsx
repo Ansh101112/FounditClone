ed743f29e93d4bdcfaac75295ce00aaa734c3989
Anshuman <tanshuman145@gmail.com>
2024-03-27 16:58:21 +0530

    resolving errors

--- src/Pages/JobModal.jsx
+++ src/Pages/JobModal.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Box,Grid,FilledInput,Select,MenuItem,Dialog,DialogTitle,DialogContent} from '@mui/material'
 import Navbar from '../components/Navbar'
 
 const JobModal = () => {
   return (
     <>
         <Navbar></Navbar>
-        <p>in develeopmemt</p>
+        <Dialog>
+           
+        </Dialog>
     </>
   )
 }
